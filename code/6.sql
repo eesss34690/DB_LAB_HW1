0fc31bcbbcf5c9e80a76e0cc307f7f0f79bf5320
eesss34690 <eesss.ee07@nctu.edu.tw>
2020-04-16 11:26:07 +0800

    6 change, 0416

--- code/6.sql
+++ code/6.sql
@@ -1,7 +1,7 @@
 SELECT MA.position, C.champion_name
 FROM champ C,
 (
-    SELECT D.position, MAX(D.cnt) AS cnt
+    SELECT D.position, MAX(D.cnt) OVER (PARTITION BY D.position)AS ma, cnt, D.champion_id
     FROM
     (
         SELECT P.position, P.champion_id, COUNT(P.player_id) AS cnt
@@ -14,19 +14,7 @@ FROM champ C,
         WHERE M.duration BETWEEN 2399 AND 3001 AND M.match_id = P.match_id
         GROUP BY P.position, P.champion_id
     ) D
-    GROUP BY D.position
-) MA,
-(
-    SELECT P.position, P.champion_id, COUNT(P.player_id) AS cnt
-    FROM match_info M, 
-    (
-        SELECT position, champion_id, player_id, match_id
-        FROM participant
-        WHERE position = 'DUO_CARRY' OR position = 'DUO_SUPPORT'OR position = 'JUNGLE'OR position = 'MID'OR position = 'TOP' 
-    ) P
-    WHERE M.duration BETWEEN 2399 AND 3001 AND M.match_id = P.match_id
-    GROUP BY P.position, P.champion_id
-)AL
-WHERE MA.position = AL.position AND MA.cnt = AL.cnt AND AL.champion_id = C.champion_id
+) MA
+WHERE MA.cnt = AL.cnt AND MA.champion_id = C.champion_id
 ORDER BY FIELD(MA.position, 'DUO_CARRY', 'DUO_SUPPORT', 'JUNGLE', 'MID', 'TOP')
 ;
