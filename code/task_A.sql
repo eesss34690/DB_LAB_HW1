5d5f19631fa53d96535cba0d4a92db8382b5e549
eesss34690 <eesss.ee07@nctu.edu.tw>
2020-04-20 07:47:21 +0800

    0420-table

--- code/task_A.sql
+++ code/task_A.sql
@@ -1,9 +1,9 @@
 
 CREATE TABLE champ(
-     champion_name VARCHAR(12) NOT NULL,
+     champion_name VARCHAR(15) NOT NULL,
      champion_id int NOT NULL,
      PRIMARY KEY (champion_id)
-);
+);/*
 CREATE TABLE match_info(
      match_id int NOT NULL,
      duration int,
@@ -86,4 +86,4 @@ fields terminated by ','
 enclosed by '"'
 lines terminated by '\r\n'
 ignore 1 lines;
-
+*/
