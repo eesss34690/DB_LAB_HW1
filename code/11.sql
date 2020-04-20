f455f97bc2b305ba98572fe158b02d167ce480e4
eesss34690 <eesss.ee07@nctu.edu.tw>
2020-04-20 08:17:49 +0800

    0420, 11 unfinished

--- code/11.sql
+++ code/11.sql
@@ -1,3 +1,4 @@
+(
 SELECT P.ss2,
     SUM(CASE 
         WHEN S.win = 1 THEN 1
@@ -6,4 +7,17 @@ SELECT P.ss2,
 FROM stat S, participant P
 WHERE S.player_id = P.player_id AND P.ss1 = 'Flash'
 GROUP BY P.ss2
-HAVING P.ss2 = 'Ignite' OR P.ss2 = 'Teleport';
+HAVING P.ss2 = 'Ignite' OR P.ss2 = 'Teleport'
+)G11,
+(
+SELECT P.ss1,
+    SUM(CASE 
+        WHEN S.win = 1 THEN 1
+        WHEN S.win = 0 THEN 0
+        END) / COUNT(S.win) AS win_ratio
+FROM stat S, participant P
+WHERE S.player_id = P.player_id AND P.ss2 = 'Flash'
+GROUP BY P.ss1
+HAVING P.ss1 = 'Ignite' OR P.ss1 = 'Teleport'
+)G22,
+;
